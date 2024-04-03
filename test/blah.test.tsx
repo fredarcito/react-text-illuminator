47c3c3c76ff324e3d8edb053647676cce7ebda08
fredarcito <fredar21@gmail.com>
2024-04-03 14:42:29 -0500

    update react version

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import HighlightText from '../src/index';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <HighlightText
+        textContent="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec justo eu velit lacinia dignissim. Integer nec pretium felis."
+        highlightTerm="Lorem"
+        highlightColor="yellow"
+      />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
