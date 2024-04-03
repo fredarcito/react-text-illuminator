9d857ea3aab7037030193d49051545143ee0869b
fredarcito <fredar21@gmail.com>
2024-04-03 13:30:51 -0500

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface HighlightTextProps {
+  textContent: string;
+  highlightTerm?: string;
+  highlightColor?: string;
+}
+
+const HighlightText: React.FC<HighlightTextProps> = ({
+  textContent,
+  highlightTerm = '',
+  highlightColor = 'yellow',
+}) => {
+  const highlight = (
+    textContent: string,
+    highlightTerm: string
+  ): JSX.Element[] => {
+    const escapeRegExp = (string: string) => {
+      return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    };
+
+    if (!highlightTerm.trim() || highlightTerm === '') {
+      return [<span key="0">{textContent}</span>];
+    }
+
+    const textEscaped = escapeRegExp(highlightTerm);
+
+    const regex = new RegExp(`(${textEscaped})`, 'gi');
+
+    const splits = textContent.split(regex);
+
+    return splits.map((split, index) =>
+      regex.test(split.toLowerCase()) ? (
+        <span key={index} style={{ backgroundColor: highlightColor }}>
+          {split}
+        </span>
+      ) : (
+        <span key={index}>{split}</span>
+      )
+    );
+  };
+
+  return <p>{highlight(textContent, highlightTerm)}</p>;
+};
+
+export default HighlightText;
